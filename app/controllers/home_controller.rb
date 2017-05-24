74da71db311404455f51b5aa258a42de2459f483
Grayson Sinclair <highwind360@gmail.com>
2017-05-24 09:50:22 -0700

    Integrated a static template for the site's landing page into a rails view.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  # Application Root
+  def index
+    @bodyclass = "landing"
+  end
+end
---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionDispatch::IntegrationTest
+  # TODO: make sure that it renders the correct pages for logged in/out users
+  # test "the truth" do
+  #   assert true
+  # end
+end
